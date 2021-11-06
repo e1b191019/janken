a2880d25622cbc85149ea1bdd1d5d210e0536fd9
B191019 <e1b19019@st.oit.ac.jp>
2021-11-06 16:57:27 +0900

    insertの実装

---
+++ src/main/java/oit/is/z0779/kaizi/model/UserMapper.java
@@ -0,0 +1,21 @@
+package oit.is.z0779.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+  @Select("SELECT * from users where id = #{id}")
+  User selectById(int id);
+
+  @Select("SELECT * from users where name = #{name}")
+  ArrayList<User> selectByName(String name);
+
+  @Select("SELECT * FROM USERS")
+  ArrayList<User> selectAllUser();
+}
--- src/main/java/oit/is/z0779/kaizi/security/Lec03AuthConfiguration.java
+++ src/main/java/oit/is/z0779/kaizi/security/Lec03AuthConfiguration.java
@@ -46,5 +46,6 @@ public class Lec03AuthConfiguration extends WebSecurityConfigurerAdapter {
 
     http.csrf().disable();
     http.headers().frameOptions().disable();
+
   }
 }
